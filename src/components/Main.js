40c7a2d3c0cdb25108c8cfaf8a2e1e4ec36e1f83
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-11-18 16:24:04 +0800

    ajax请求测试

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import Test from 'components/Test';
+import GetJson from 'components/Test';
 let yeomanImage = require('../images/yeoman.png');
 
 
@@ -10,7 +10,7 @@ class AppComponent extends React.Component {
     render() {
         return (
             <div className="index">
-                <Test/>
+                <GetJson/>
                 <img src={yeomanImage} alt="Yeoman Generator"/>
                 <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
             </div>
--- src/components/test.js
+++ src/components/test.js
@@ -1,10 +1,39 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('es6-promise').polyfill();
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+// import React from 'react';
 
-import React, { PropTypes } from 'react';
 
+class GetJsonComponent extends React.Component {
+    componentWillMount(){
+        // alert(111);
+    }
+    componentDidMount() {
+        function status(response) {
+            if (response.status >= 200 && response.status < 300) {
+                return response
+            }
+            throw new Error(response.statusText)
+        }
 
-class TestComponent extends React.Component {
+        function json(response) {
+            return response.json()
+        }
+
+        fetch('https://api.meixinglobal.com/web/product/first_page')
+            .then(function(response) {
+                if (response.status >= 400) {
+                    throw new Error("Bad response from server");
+                }
+                return response.json();
+            })
+            .then(function(stories) {
+                console.log(stories);
+            });
+    }
+
     render() {
         return (
             <div>
@@ -14,7 +43,6 @@ class TestComponent extends React.Component {
     }
 }
 
-TestComponent.defaultProps = {
-};
+GetJsonComponent.defaultProps = {};
 
-export default TestComponent;
+export default GetJsonComponent;
